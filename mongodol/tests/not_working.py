fb72a8c0f559b982b2b273a7c7948fb15db81098
Thor Whalen <thorwhalen1@gmail.com>
2021-03-24 12:34:40 -0700

    a test that's not working yet

---
+++ mongodol/tests/not_working.py
@@ -0,0 +1,26 @@
+import pytest
+from mongodol.tests.util import populated_pymongo_collection
+from mongodol.base import MongoCollectionReader
+
+
+def test_mongo_values_view_when_wrapping():
+    s = MongoCollectionReader(mgc=populated_pymongo_collection())
+
+    assert type(s.values()).__name__ == 'MongoValuesView'
+
+    from py2store import wrap_kvs
+
+    ss = wrap_kvs(s)
+    assert type(ss.values()).__name__ == 'MongoValuesView'
+
+    from py2store import wrap_kvs
+
+    ss = wrap_kvs(s)
+    ss.values()
+
+    # TODO: But we want this to NOT raise an error. Need to use factories
+    with pytest.raises(AttributeError) as excinfo:
+        list(ss.values())  # BOOM
+
+    assert "'ValuesView' object has no attribute 'mgc'" in str(excinfo.value)
+
