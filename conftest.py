f428f62b547dee32f56e0ed4d6a3ce81204e686c
GitHub CI Runner <thorwhalen1@gmail.com>
2021-11-10 17:53:21 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
 import os
 import pathlib
- 
- 
+
+
 def pytest_ignore_collect(path):
-   root_dir = pathlib.Path(__file__).parent.resolve()
-   scrap_dir = pathlib.PurePath(root_dir, 'mongodol', 'scrap')
-   return str(scrap_dir) in str(path)
+    root_dir = pathlib.Path(__file__).parent.resolve()
+    scrap_dir = pathlib.PurePath(root_dir, 'mongodol', 'scrap')
+    return str(scrap_dir) in str(path)
